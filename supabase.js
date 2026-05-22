47590c32667fce34160e5f32d7f799d777801cd5
mcteodoro <161388910+mcteodoro@users.noreply.github.com>
2026-05-22 11:12:38 -0300

    primeira versão painel promoters

---
+++ supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://xbteuroldclsiaskqokb.supabase.co"
+
+const SUPABASE_KEY = "sb_publishable_M96nf7rmin03-S-kKCwDCA_0dylrQbN"
+const db = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
+
+
+
